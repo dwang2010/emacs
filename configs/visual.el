691ef7f13bc8bf0cc9043df810c63a3b0acc4d74
darren wang <darren.wang@gmail.com>
2023-11-19 23:33:22 -0800

    test: migrate to mood-line for mode-line

--- configs/visual.el
+++ configs/visual.el
@@ -26,33 +26,10 @@
 ;; ------------------------------------------------------------------------
 ;; mode line customization
 ;; ------------------------------------------------------------------------
-(setq-default display-time-format "(%a, %m/%d) | %I:%M %p") ; display time
-(setq-default display-time-mail-string "")           ; remove mail notice
-(setq-default display-time-default-load-average nil) ; remove load average
-(display-time-mode nil)
-
-(let ((standard-mode-line-format
-       (list "%e"
-             'mode-line-front-space
-             'mode-line-mule-info
-             'mode-line-client
-             'mode-line-modified
-             'mode-line-remote
-             'mode-line-frame-identification
-             ;'mode-line-buffer-identification
-             ;"   "
-             '(:eval (propertize "%b  " 'face 'mode-line-buffer-id))
-             ;'mode-line-position
-             "(%l,%c)  "
-             ;'(:eval (propertize "(%l,%c)  " 'face 'mode-line))
-             ;'(vc-mode vc-mode)
-             ;"  "
-             ;'mode-line-modes
-             '(:eval (propertize "%m  " 'face 'shadow))
-             'mode-line-misc-info
-             ;'(:eval (propertize (format-time-string "(%a, %m/%d | %I:%M %p)") 'face 'shadow))
-             'mode-line-end-spaces)))
-  (setq-default mode-line-format standard-mode-line-format))
+(use-package mood-line
+  :ensure t
+  :config (mood-line-mode)
+  (setq-default mood-line-glyph-alist mood-line-glyphs-ascii))
 
 ;; ------------------------------------------------------------------------
 ;; display customization
