06dd8be547e8494b05429f9f2053ce87e0d124a2
darren wang <darren.wang@gmail.com>
2024-01-20 12:20:37 -0800

    move configs for native packages to separate file

---
+++ configs/packs_native.el
@@ -0,0 +1,76 @@
+;; ------------------------------------------------------------------------
+;; project based file management (native)
+;; ------------------------------------------------------------------------
+(use-package project ; test native
+  ;; native binds
+  ;; C-x p d == (dired to target dir)
+  ;; C-x p D == (dired to root dir)
+  :config
+  (setq-default project-vc-ignores '()) ; list of stuff to ignore
+  (setq-default project-switch-commands
+                '((project-find-file "Find file" nil)
+                  (project-find-dir "Find directory" nil))))
+
+;; ------------------------------------------------------------------------
+;; tree sitter - syntax highlighting (native in emacs >= 29)
+;; ------------------------------------------------------------------------
+(use-package treesit
+  :config
+  (setq-default treesit-font-lock-level 4))
+
+;; deal with the new major modes
+;; https://github.com/renzmann/treesit-auto
+(use-package treesit-auto
+  :ensure t
+  :config
+  (setq-default treesit-auto-install 'prompt)
+  (global-treesit-auto-mode))
+
+;; ------------------------------------------------------------------------
+;; dired - file / directory explorer (native)
+;; ------------------------------------------------------------------------
+(use-package dired
+  :bind ("C-x C-d" . 'dired-jump) ; jump straight to dir of current buffer
+  :hook (dired-mode . dired-hide-details-mode) ; collapsed extra info, show via "("
+  :config
+  (setq-default dired-dwim-target t) ; target operation to other dired window
+  (setq-default dired-listing-switches "-laGh --group-directories-first -v")
+  (setq-default dired-kill-when-opening-new-dired-buffer t))
+
+(require 'ls-lisp)
+(setq-default ls-lisp-use-insert-directory-program nil)
+(setq-default ls-lisp-dirs-first t)
+
+;; ------------------------------------------------------------------------
+;; paren - highlight matching braces (native)
+;; ------------------------------------------------------------------------
+(use-package paren
+  :config (show-paren-mode 1)
+  (setq-default show-paren-delay 0))
+
+;; ------------------------------------------------------------------------
+;; highlight current line only in specific modes (native)
+;; ------------------------------------------------------------------------
+(use-package hl-line
+  :hook ((prog-mode . hl-line-mode)
+         (text-mode . hl-line-mode)))
+
+;; ------------------------------------------------------------------------
+;; hideshow - code folding (native)
+;; ------------------------------------------------------------------------
+(use-package hideshow
+  :bind (("<C-tab>" . hs-toggle-hiding)
+         ("<C-M-tab>" . 'hs-hide-all))
+  :hook ((prog-mode . hs-minor-mode)
+         (c-mode-common . hs-minor-mode)))
+
+;; ------------------------------------------------------------------------
+;; flyspell - spell checking (native)
+;; ispell needs to be locally installed (not a part of emacs)
+;; ------------------------------------------------------------------------
+(use-package flyspell
+  :bind ("C-c C-4" . 'flyspell-mode)
+  :config (setq-default flyspell-issue-message-flag nil))
+
+;; M-$ = Check and correct spelling of the word at point
+;; flyspell-buffer to check spelling of entire buffer
--- init.el
+++ init.el
@@ -17,8 +17,9 @@
 ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Lisp-Libraries.html
 ;; ------------------------------------------------------------------------
 (add-to-list 'load-path "~/.emacs.d/configs/")
-(load-library "base")        ; basic settings + keybindings
-(load-library "visual")      ; themes / display related
-(load-library "programming") ; language related default settings
-(load-library "packages")    ; blob package customization
-(load-library "org_cfg")     ; org-mode related configs
+(load-library "base")         ; basic settings + keybindings
+(load-library "visual")       ; themes / display related
+(load-library "programming")  ; language related default settings
+(load-library "packs_native") ; native package configs
+(load-library "packs_ext")    ; third-party package configs
+(load-library "org_cfg")      ; org-mode related configs
