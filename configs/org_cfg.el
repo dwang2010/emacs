ff6853f55c421aa091ba29dc779b78b88adece63
Darren Wang <dcwang@meta.com>
2025-10-27 11:54:59 -0700

    'C-b' to org-emphasize, remove the custom function

--- configs/org_cfg.el
+++ configs/org_cfg.el
@@ -7,9 +7,9 @@
 (defun my-org-binding-configs ()
   (local-unset-key (kbd "C-,")) ;; cycle org agenda files
   (local-unset-key (kbd "M-h")) ;; org-mark-element
-  (local-set-key (kbd "C-c b") 'my-quick-add-emphasis)
   (local-set-key (kbd "C-c ,") 'my-quick-add-src-blk)
-  (local-set-key (kbd "C-c C-,") 'my-quick-add-src-blk))
+  (local-set-key (kbd "C-c C-,") 'my-quick-add-src-blk)
+  (local-set-key (kbd "C-b") 'org-emphasize))
 
 (use-package org
   :bind ("C-c C-o" . org-open-at-point-global)
@@ -313,12 +313,6 @@
                         (time-subtract
                          (encode-time (my-get-next-sunday))
                          (days-to-time (* 7 weeks))))))
-
-(defun my-quick-add-emphasis ()
-  "Enclose symbol with '=' for org emphasis"
-  (interactive)
-  (require 'expand-region)
-  (er/mark-symbol) (org-emphasize))
 
 (defun my-quick-add-src-blk ()
   "Auto add source block without prompting"
