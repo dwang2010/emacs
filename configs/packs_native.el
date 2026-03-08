eba9b24ce062340b32085ad841634db868caa99b
Darren Wang <dcwang@meta.com>
2026-03-08 00:57:55 -0800

    fix the treesitter settings - still wonky tho

--- configs/packs_native.el
+++ configs/packs_native.el
@@ -12,57 +12,40 @@
 ;; ------------------------------------------------------------------------
 ;; tree sitter - syntax highlighting (native in emacs >= 29)
 ;; ------------------------------------------------------------------------
-;; (use-package treesit
-;;   :mode (("\\.js\\'"  . typescript-ts-mode)
-;;          ("\\.ts\\'"  . typescript-ts-mode)
-;;          ("\\.jsx\\'"  . tsx-ts-mode)
-;;          ("\\.tsx\\'"  . tsx-ts-mode)
-;;          ("\\.json\\'" .  json-ts-mode))
-;;   :preface
-;;   (defun os/setup-install-grammars ()
-;;     "Install Tree-sitter grammars if they are absent."
-;;     (interactive)
-;;     (dolist (grammar
-;;              '((css . ("https://github.com/tree-sitter/tree-sitter-css"))
-;;                (html . ("https://github.com/tree-sitter/tree-sitter-html"))
-;;                (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript"))
-;;                (json . ("https://github.com/tree-sitter/tree-sitter-json"))
-;;                (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src"))
-;;                (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src"))
-;;                (bash "https://github.com/tree-sitter/tree-sitter-bash")
-;;                (python . ("https://github.com/tree-sitter/tree-sitter-python"))
-;;                (go "https://github.com/tree-sitter/tree-sitter-go")
-;;                (make "https://github.com/alemuller/tree-sitter-make")
-;;                (elisp "https://github.com/Wilfred/tree-sitter-elisp")))
-;;       (add-to-list 'treesit-language-source-alist grammar)
-;;       ;; Only install `grammar' if we don't already have it
-;;       ;; installed. However, if you want to *update* a grammar then
-;;       ;; this obviously prevents that from happening.
-;;       (unless (treesit-language-available-p (car grammar))
-;;         (treesit-install-language-grammar (car grammar)))))
+(use-package treesit
+  :preface
+  (defun my/setup-treesit-grammars ()
+    "Install Tree-sitter grammars if they are absent."
+    (interactive)
+    (dolist (grammar
+             '((javascript . ("https://github.com/tree-sitter/tree-sitter-javascript"))
+               (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src"))
+               (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src"))
+               (python . ("https://github.com/tree-sitter/tree-sitter-python"))
+               (bash . ("https://github.com/tree-sitter/tree-sitter-bash"))
+               (json . ("https://github.com/tree-sitter/tree-sitter-json"))))
+      (add-to-list 'treesit-language-source-alist grammar)
+      (unless (treesit-language-available-p (car grammar))
+        (treesit-install-language-grammar (car grammar)))))
 
-;;   ;; Optional, but recommended. Tree-sitter enabled major modes are
-;;   ;; distinct from their ordinary counterparts.
+  :config
+  (my/setup-treesit-grammars)
+  (setq-default treesit-font-lock-level 4)
 
-;;   ;; You can remap major modes with `major-mode-remap-alist'. Note
-;;   ;; that this does *not* extend to hooks! Make sure you migrate them
-;;   ;; also
-;;   (dolist (mapping
-;;            '((python-mode . python-ts-mode)
-;;              (css-mode . css-ts-mode)
-;;              (typescript-mode . typescript-ts-mode)
-;;              (js-mode . typescript-ts-mode)
-;;              (js2-mode . typescript-ts-mode)
-;;              (bash-mode . bash-ts-mode)
-;;              (css-mode . css-ts-mode)
-;;              (json-mode . json-ts-mode)
-;;              (js-json-mode . json-ts-mode)
-;;              (sh-mode . bash-ts-mode)
-;;              (sh-base-mode . bash-ts-mode)))
-;;     (add-to-list 'major-mode-remap-alist mapping))
-;;   :config
-;;   (os/setup-install-grammars)
-;;   (setq-default treesit-font-lock-level 4))
+  ;; remap traditional modes to tree-sitter counterparts
+  (dolist (mapping
+           '((python-mode . python-ts-mode)
+             (js-mode . js-ts-mode)
+             (js-jsx-mode . js-ts-mode)
+             (js-json-mode . json-ts-mode)
+             (sh-mode . bash-ts-mode)))
+    (add-to-list 'major-mode-remap-alist mapping))
+
+  ;; file types with no traditional mode to remap from
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode)))
 
 ;; ------------------------------------------------------------------------
 ;; dired - file / directory explorer (native)
